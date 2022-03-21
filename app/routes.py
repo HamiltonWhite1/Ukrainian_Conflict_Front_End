9d88d026fb4345e82ab6c84a943c0b872fd9d8c0
HamiltonWhite1 <92882456+HamiltonWhite1@users.noreply.github.com>
2022-03-21 15:38:33 -0400

    Conclusion is written, review on Wednesday/Thursday to clean language choices up.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,6 @@
 from flask import render_template, send_file
 from app import app
 
-
 @app.route('/') 
 @app.route('/index')
 def index():
@@ -16,24 +15,26 @@ def total_sentiment():
 @app.route('/left_sentiment', methods = ['GET', 'POST'] )
 def left_sentiment():
     left_labels = ['Hostile Language', 'Defensive Language', 'Overlapping Language', 'Unrelated Discussion']
-    left_values = [126, 246, 192, 5]
+    left_values = [79, 164, 110, 2]
     return render_template('left_sentiment.html', labels=left_labels, values=left_values)
 
 @app.route('/right_sentiment', methods = ['GET', 'POST'] )
 def right_sentiment():
     right_labels = ['Hostile Language', 'Defensive Language', 'Overlapping Language', 'Unrelated Discussion']
-    right_values = [169, 249, 212, 0]
+    right_values = [93, 147, 112, 1]
     return render_template('right_sentiment.html', labels=right_labels, values=right_values)
 
 @app.route('/activity', methods = ['GET', 'POST'] )
 def activity():
-    return render_template('activity.html', title='Activity')
+    from app.results import m_time_list, m_count_list
+    activity_labels = m_time_list
+    activity_values = m_count_list
+    return render_template('activity.html', labels=activity_labels, values=activity_values)
 
 @app.route('/raw_data_collection', methods = ['GET', 'POST'] )
 def raw_data():
     p = "downloadable_data.json.zip"
     return send_file(p, as_attachment=True)
-
 
 @app.route('/methods', methods = ['GET', 'POST'] )
 def study_methods():
